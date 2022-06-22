2305915b221d7ae627cadd068602f335534690ea
kibuikaCodes <kibuikasteve@gmail.com>
2022-06-22 19:32:01 +0300

    complete build

---
+++ components/common/Layout/Layout.js
@@ -0,0 +1,88 @@
+import { useState, useEffect } from "react";
+import clsx from "clsx";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
+import { useTheme } from "next-themes";
+// import Image from "next/image";
+
+export function Layout({ children }) {
+  return (
+    <div className="w-full min-h-screen dark:bg-gray-700 dark:text-white">
+      <div className="max-w-screen-sm px-4 py-12 mx-auto antialiased font-body">
+        <Header />
+        <main>{children}</main>
+        <footer className="text-lg font-light">
+          <a href="https://twitter.com/the_kibuika" target="_blank" rel="noreferrer">twitter</a>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+const Header = () => {
+  const { setTheme, resolvedTheme } = useTheme();
+  const { pathname } = useRouter();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  const toggleDarkMode = (checked) => {
+    const isDarkMode = checked;
+
+    if (isDarkMode) setTheme("dark");
+    else setTheme("light");
+  };
+
+  const isRoot = pathname === "/";
+  const isDarkMode = resolvedTheme === "dark";
+
+  return (
+    <header
+      className={clsx("flex items-center justify-between ", {
+        "mb-8": isRoot,
+        "mb-2": !isRoot,
+      })}
+    >
+      <div className={"max-w-md"}>
+        {isRoot ? <LargeTitle /> : <SmallTitle />}
+      </div>
+      {mounted && (
+        <div className="flex space-x-4">
+          <DarkModeSwitch checked={isDarkMode} onChange={toggleDarkMode} />
+        </div>
+      )}
+    </header>
+  );
+};
+
+const LargeTitle = () => (
+  <h1>
+    <Link href="/">
+      <a
+        className={clsx(
+          "text-3xl font-black leading-none text-black no-underline font-display",
+          "sm:text-5xl",
+          "dark:text-white"
+        )}
+      >
+        Kibuika
+      </a>
+    </Link>
+  </h1>
+);
+
+const SmallTitle = () => (
+  <h1>
+    <Link href="/">
+      <a
+        className={clsx(
+          "text-2xl font-black text-black no-underline font-display",
+          "dark:text-white"
+        )}
+      >
+        Kibuika
+      </a>
+    </Link>
+  </h1>
+);
